686f9faebe262a4056e10d2838f2dc9587260d48
smeghead <smeghead7@gmail.com>
2010-12-19 02:03:09 +0900

    サイトIDを変更していない部分があったので変更した。

--- src/background/popup.js
+++ src/background/popup.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
   var BG = chrome.extension.getBackgroundPage();
   var iine = BG.getIINE();
-  var div_iine = '<div id="nsc_iine" class="nsc_iine" siteId="4ZQ4" targetUrl="' + iine.encoded_url  + 
+  var div_iine = '<div id="nsc_iine" class="nsc_iine" siteId="57DZ" targetUrl="' + iine.encoded_url  + 
             '" targetTitle="' + iine.title + '"></div>';
-  var div_cmt = '<div id="nsc_comments" class="nsc_comments" siteId="4ZQ4" targetUrl="' + iine.encoded_url  + 
+  var div_cmt = '<div id="nsc_comments" class="nsc_comments" siteId="57DZ" targetUrl="' + iine.encoded_url  + 
             '" targetTitle="' + iine.title + '"></div>';
 
-  $('#page_title').text(iine.title || 'empty');
-  $('#page_url').text(iine.url || 'empty');
+  $('#page_title').text(iine.title || '(empty)');
+  $('#page_url').text(iine.url || '(empty)');
 
   $('#buttons').append(div_iine);
   $('#buttons').append(div_cmt);
