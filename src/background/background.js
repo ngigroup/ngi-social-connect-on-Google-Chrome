dd587f46a8603abe8e9f64d6b0fdaa29f2b4b615
smeghead <smeghead7@gmail.com>
2010-12-25 16:25:21 +0900

    マルチウィンドウの場合に、ページタイトル・URLを正しく取得できない場合があるのを修正した。ページタイトル・URLの取得方法を大幅に変更した。

--- src/background/background.js
+++ src/background/background.js
@@ -1,45 +1,14 @@
-function IINE(){
-}
-IINE.prototype = {
-  set:function _set(url, title){
-    this.url = url;
-    this.encoded_url = encodeURIComponent(url);
-    this.title = title;
-  },
-  get:function _get() {
-      return {url: this.url, title: this.title, encoded_url: this.encoded_url};
-  }
-};
-var iine = new IINE();
-
-function getIINE() {
-    return iine.get();
-}
-
-var run_on_tab = function(tab){
-  var url = tab.url;
-  if (!/^http/.test(url)) return;
-  if (url.length > 255) return;
-  iine.set(tab.url, tab.title);
-};
-
-var current_window;
-chrome.windows.getCurrent(function(window){
-  current_window = window;
-});
-
-chrome.tabs.onSelectionChanged.addListener(function(tabid){
-  chrome.tabs.get(tabid, function(tab){
-    if (tab.windowId === current_window.id) {
-      run_on_tab(tab);
-    }
+function get_current_page_information(callback) {
+  var page_info = {title: '', url: ''};
+  chrome.tabs.getSelected(undefined, function(tab){
+    var url = tab.url;
+    if (!/^http/.test(url)) return;
+    if (url.length > 255) return;
+    page_info = {
+      title: tab.title,
+      url: url
+    };
+    callback(page_info);
   });
-});
-
-chrome.tabs.onUpdated.addListener(function(tabid, inf){
-    chrome.tabs.getSelected(current_window.id, function(tab){
-      run_on_tab(tab);
-    });
-});
-
+}
 /* vim: set ts=2 sw=2 sts=2 expandtab fenc=utf-8: */
--- src/background/popup.js
+++ src/background/popup.js
@@ -1,30 +1,31 @@
 var siteId = '57DZ';
 
 $(document).ready(function() {
-  var iine = chrome.extension.getBackgroundPage().getIINE();
-  // tag string templates.
-  var div_iine = '<div id="nsc_iine" class="nsc_iine" siteId="' + siteId + '" targetUrl="' + iine.encoded_url  + 
-            '" targetTitle="' + iine.title + '"></div>';
-  var div_cmt = '<div id="nsc_comments" class="nsc_comments" siteId="' + siteId + '" targetUrl="' + iine.encoded_url  + 
-            '" targetTitle="' + iine.title + '"></div>';
-  var friend_list = '<div id="nsc_friendlist" siteId="' + siteId + '" height="300" width="300"></div>';
-  var widgets_script = '<script type="text/javascript" src="http://ngi.s3.amazonaws.com/nsc-widgets.js"></script>';
+  chrome.extension.getBackgroundPage().get_current_page_information(function(page_info){
+    // tag string templates.
+    var div_iine = '<div id="nsc_iine" class="nsc_iine" siteId="' + siteId + '" targetUrl="' + encodeURIComponent(page_info.url)  + 
+              '" targetTitle="' + page_info.title + '"></div>';
+    var div_cmt = '<div id="nsc_comments" class="nsc_comments" siteId="' + siteId + '" targetUrl="' + encodeURIComponent(page_info.url)  + 
+              '" targetTitle="' + page_info.title + '"></div>';
+    var friend_list = '<div id="nsc_friendlist" siteId="' + siteId + '" height="300" width="300"></div>';
+    var widgets_script = '<script type="text/javascript" src="http://ngi.s3.amazonaws.com/nsc-widgets.js"></script>';
 
-  // insert page information.
-  $('#page_title').text(iine.title || '(empty)');
-  $('#page_url').text(iine.url || '(empty)');
+    // insert page information.
+    $('#page_title').text(page_info.title || '(empty)');
+    $('#page_url').text(page_info.url || '(empty)');
 
-  // insert iine and comment buttons.
-  $('#buttons').append(div_iine);
-  $('#buttons').append(div_cmt);
+    // insert iine and comment buttons.
+    $('#buttons').append(div_iine);
+    $('#buttons').append(div_cmt);
 
-  // insert friend list.
-  $('body').append(friend_list);
+    // insert friend list.
+    $('body').append(friend_list);
 
-  // insert widgets script.
-  window.setTimeout(function(){
-    $('head').append(widgets_script);
-  }, 10);
+    // insert widgets script.
+    window.setTimeout(function(){
+      $('head').append(widgets_script);
+    }, 10);
+  });
 });
 
 /* vim: set ts=2 sw=2 sts=2 expandtab fenc=utf-8: */
